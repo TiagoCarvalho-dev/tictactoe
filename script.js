766b2b80c3d30bcbf17ae7279c1a89459943fdb7
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-08-16 22:23:42 -0300

    Changed a few important functions
    First the Players() was removed and incorporated in the GameLoop(), that way the switchActivePlayer() works as intended.
    Also added a function to the Board() to mark a square on the board.
    And finally added a loop to check if everything works fine.

--- script.js
+++ script.js
@@ -2,16 +2,54 @@ const Board = function() {
   const rows = 3;
   const column = 3;
   const board = [];
-  const buildBoard = function() {
-    for(let i = 0; i < rows; i++) {
-      board.push([]);
-      for(let j = 0; j < column; j++) {
-        board[i].push('0');
-      }
+
+  for(let i = 0; i < rows; i++) {
+    board.push([]);
+    for(let j = 0; j < column; j++) {
+      board[i].push('0');
     }
   }
-  console.log(board);
-  return {buildBoard};
+
+  const getBoard = () => board;
+
+  const markSquare = function(playerMarker, row, column, board) {
+    board[row][column] = playerMarker;
+  }
+
+  return {getBoard, markSquare};
 }
 
-Board().buildBoard();
+const GameLoop = function() {
+  const board = Board();
+
+  const players = [{
+    name: 'PlayerOne',
+    marker: '1'
+  },
+  {
+    name: 'PlayerTwo',
+    marker: '2'
+  }];
+
+  let activePlayer = players[0];
+
+  const switchActivePlayer = () => {
+    activePlayer = activePlayer === players[0] ? players[1] : players[0];
+  }
+
+  const playGame = () => {
+    console.log('Hello, let\'s play');
+    for(let i = 0; i < 4; i++) {
+      console.log(JSON.stringify(board.getBoard()));
+      console.log(`It\'s ${activePlayer.name}'s turn!`);
+      let row = prompt('Please choose the row');
+      let column = prompt('Please choose the column');
+      board.markSquare(activePlayer.marker, row, column, board.getBoard());
+      switchActivePlayer();
+    }
+  }
+
+  const getActivePlayer = () => activePlayer;
+
+  return {playGame, getActivePlayer};
+}
