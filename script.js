95eb682df9edcd42480852c5c6ca2be16d2a4333
Tiago de Siqueira Carvalho <tiagosiqueira.dev@gmail.com>
2023-08-29 21:39:35 -0300

    Fixed the checkBoard function to return when a result is found

--- script.js
+++ script.js
@@ -107,17 +107,17 @@ const GameLoop = function(playerOne = 'Player One', playerTwo = 'Player Two') {
   const checkBoard = (board) => {
     for(let i = 0; i < 3; i++) {
       if(board[i][0] === board[i][1] && board[i][1] === board[i][2]) {
-        if(board[i][0] === 'X') victoriousPlayer = players[0];
-        if(board[i][0] === 'O') victoriousPlayer = players[1];
+        if(board[i][0] === 'X') return victoriousPlayer = players[0];
+        if(board[i][0] === 'O') return victoriousPlayer = players[1];
       } else if(board[0][i] === board[1][i] && board[1][i] === board[2][i]) {
-        if(board[0][i] === 'X') victoriousPlayer = players[0];
-        if(board[0][i] === 'O') victoriousPlayer = players[1];
+        if(board[0][i] === 'X') return victoriousPlayer = players[0];
+        if(board[0][i] === 'O') return victoriousPlayer = players[1];
       }
     }
     if((board[0][0] === board[1][1] && board[1][1] === board[2][2]) ||
        (board[0][2] === board[1][1] && board[1][1] === board[2][0])) {
-        if(board[1][1] === 'X') victoriousPlayer = players[0];
-        if(board[1][1] === 'O') victoriousPlayer = players[1];
+        if(board[1][1] === 'X') return victoriousPlayer = players[0];
+        if(board[1][1] === 'O') return victoriousPlayer = players[1];
     }
     if(board[0][0] !== '' && board[0][1] !== '' && board[0][2] !== '' &&
        board[1][0] !== '' && board[1][1] !== '' && board[1][2] !== '' &&
